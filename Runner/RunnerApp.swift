f616bb2e4579bac4b5635f5699be4a2a45dbb4f3
leeyuno <leeyuno@kdiwin.com>
2023-10-12 16:09:35 +0900

    Initial Commit

---
+++ Runner/RunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnerApp.swift
+//  Runner
+//
+//  Created by 이윤오 on 2023/10/12.
+//
+
+import SwiftUI
+
+@main
+struct RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
